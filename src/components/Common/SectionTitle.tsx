c07316b3d23038f28a293a41b7c3f8ba3db02758
ahmadullahmukhlis <nasarimukhlis@gmail.com>
2024-12-31 17:07:38 +0430

    the problem of the is

--- src/components/Common/SectionTitle.tsx
+++ src/components/Common/SectionTitle.tsx
@@ -25,9 +25,9 @@ const SectionTitle = ({
             {subtitle}
           </span>
         )}
-        <h2 className="mb-4 text-3xl font-bold text-dark dark:text-white sm:text-4xl md:text-[40px] md:leading-[1.2]">
+        <h6 className="mb-4 text-3xl font-bold text-dark dark:text-white sm:text-4xl md:text-[40px] md:leading-[1.2]">
           {title}
-        </h2>
+        </h6>
         <p className="text-base leading-relaxed text-body-color dark:text-dark-6 sm:leading-relaxed">
           {paragraph}
         </p>
